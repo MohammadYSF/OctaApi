4a99279551f399c32e6adc354bd3518dee373726
MohammadYSF <aa.yosefiyan7@gmail.com>
2024-01-15 21:03:43 +0330

    customer , inventoryitem domain unit test

---
+++ Command/Test/Command.Domain.Test/Command.Domain.UnitTest/Customer/CustomerUnitTest.cs
@@ -0,0 +1,55 @@
+using Command.Core.Domain.Customer;
+using Command.Core.Domain.Customer.Events;
+using FluentAssertions;
+using Moq;
+
+namespace Command.Domain.UnitTest.Customer;
+
+public class CustomerUnitTest
+{
+    [Fact]
+    public void CustomerAggregate_Create_ShouldCreateNewCustomer()
+    {
+        Guid id = Guid.NewGuid();
+        int code = It.IsAny<int>();
+        string firstName = It.IsAny<string>();
+        string lastName = It.IsAny<string>();
+        string phone = It.IsAny<string>();
+        var customerAggregate = CustomerAggregate.Create(id, code, firstName, lastName, phone);
+        customerAggregate.Should().NotBeNull();
+        customerAggregate.GetDomainEvents().Should().HaveCount(1);
+        customerAggregate.GetDomainEvents()[0].Should().BeOfType<CustomerCreatedEvent>();
+    }
+    [Fact]
+    public void CustomerAggregate_AddVehicle_ShouldAddVehicleToCustomer()
+    {
+        Guid vehicleId = Guid.NewGuid();
+        string vehicleName = It.IsAny<string>();
+        string vehicleColor = It.IsAny<string>();
+        string vehiclePlate = It.IsAny<string>();
+        var customerAggregate = new CustomerAggregate();
+        customerAggregate.AddVehicle(vehicleId, vehicleName, vehiclePlate, vehicleColor);
+        customerAggregate.Should().NotBeNull();
+        customerAggregate.Vehicles.Should().HaveCount(1);
+        customerAggregate.GetDomainEvents().Should().HaveCount(1);
+        customerAggregate.GetDomainEvents()[0].Should().BeOfType<VehicleAddedToCustomerEvent>();
+
+    }
+    [Fact]
+    public void CustomerAggregate_RemoveVehicle_ShouldRemoveVehicleFromCustomer()
+    {
+        Guid vehicleId = Guid.NewGuid();
+        var customerAggregate = new CustomerAggregate();
+        customerAggregate.AddVehicle(vehicleId, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+        customerAggregate.RemoveVehicle(vehicleId);
+        customerAggregate.Vehicles.Should().HaveCount(0);
+    }
+    [Fact]
+    public void CustomerAggregate_UpdateCustomerPhoneNumber_ShouldUpdateCustomerPhoneNumber()
+    {
+        string phoneNumber = "1111111";
+        var customerAggregate = new CustomerAggregate();
+        customerAggregate.UpdateCustomerPhoneNumber(phoneNumber);
+        customerAggregate.PhoneNumber.Value.Should().Be(phoneNumber);
+    }
+}
---
+++ Command/Test/Command.Domain.Test/Command.Domain.UnitTest/InventoryItemAggregate/InventoryItemAggregateUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using Command.Core.Domain.InventoryItem;
+using Command.Core.Domain.InventoryItem.Events;
+using Command.Core.Domain.InventoryItem.ValueObjects;
+using FluentAssertions;
+using Moq;
+
+namespace Command.Domain.UnitTest.InventoryItemAggregate;
+
+public class InventoryItemAggregateUnitTest
+{
+    [Fact]
+    public void InventoryItemAggregate_Create_ShouldCreateNewInventoryItem()
+    {
+        var id = It.IsAny<Guid>();
+        var name = It.IsAny<string>();
+        var code = It.IsAny<int>();
+        var inventoryItemAggregate = InventoryItemَAggregate.Create(id, name, code);
+        inventoryItemAggregate.Should().NotBeNull();
+        inventoryItemAggregate.Id.Should().Be(id);
+        inventoryItemAggregate.Name.Value.Should().Be(name);
+        inventoryItemAggregate.Code.Value.Should().Be(code);
+        inventoryItemAggregate.GetDomainEvents().Should().HaveCount(1);
+        inventoryItemAggregate.GetDomainEvents()[0].Should().BeOfType<InventoryItemCreatedEvent>();
+    }
+    [Fact]
+    public void InventoryItemAggregate_Delete_ShouldDeleteInventoryItemAggregate()
+    {
+        var inventoryItemAggregate = new InventoryItemَAggregate();
+        inventoryItemAggregate.Delete();
+        inventoryItemAggregate.IsActive.Should().BeFalse();
+    }
+    [Fact]
+
+    public void InventoryItemAggregate_Buy_ShouldBuy()
+    {
+        var inventoryItemAggregate = new InventoryItemَAggregate();
+        inventoryItemAggregate.Name = new InventoryItemName("test");
+        inventoryItemAggregate.Code = new InventoryItemCode(10);
+        inventoryItemAggregate.Buy(3, 4, 3);
+        inventoryItemAggregate.Should().NotBeNull();
+        inventoryItemAggregate.GetDomainEvents().Should().HaveCount(1);
+        inventoryItemAggregate.GetDomainEvents()[0].Should().BeOfType<InventoryItemBoughtEvent>();
+    }
+    [Fact]
+
+    public void InventoryItemAggregate_Use_ShouldDecreaseCount()
+    {
+        var inventoryItemAggregate = InventoryItemَAggregate.Create(Guid.NewGuid(), "test", 10);
+        inventoryItemAggregate.Buy(3, 10, 100);
+        inventoryItemAggregate.Use(8);
+        inventoryItemAggregate.Count.Value.Should().Be(100 - 8);
+    }
+}
